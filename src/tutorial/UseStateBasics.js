22e97952140b088304c2de37737ba81d1b5e8aee
swati-04 <itwa0407@gmail.com>
2021-08-10 17:55:39 +0530

    advance-react usestate

---
+++ src/tutorial/UseStateBasics.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+
+function UseStateBasics() {
+    console.log(useState("Hola"));
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UseStateBasics
