89b2fa979802b021cc3ceebcdb9f192a322bd816
swati-04 <itwa0407@gmail.com>
2021-08-11 14:33:47 +0530

    use effects

--- src/tutorial/UseEffectBasics.js
+++ src/tutorial/UseEffectBasics.js
@@ -4,8 +4,11 @@ function UseEffectBasics() {
     const [value, setValue] = useState(0);
     useEffect(() => {
         console.log('call useEffect');
+        if(value>=1){//react hooks cant work conditionally
         document.title=`New Message(${value})`;
-    });
+        }
+    },[value]);
+
     console.log('render component');
 
     return (
