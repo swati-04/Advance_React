099074e85b2aed8a0954f6bd7b3f7cd717d2e684
swati-04 <itwa0407@gmail.com>
2021-08-11 16:58:46 +0530

    Form components in react

--- src/tutorial/Forms.js
+++ src/tutorial/Forms.js
@@ -11,8 +11,8 @@ function Forms() {
             console.log('submit the value');
             const person = { firstName: firstName, email: email };
             // console.log(person)
-            setPeople((people)=>{
-                return[...people,person];
+            setPeople((people) => {
+                return [...people, person];
             })
             setFirstName('');
             setEmail('');
@@ -35,6 +35,15 @@ function Forms() {
                     </div>
                     <button type="submit">Submit</button>
                 </form>
+                {people.map((person, index) => {
+                    const { id, firstName, email } = person;
+                    return (
+                        <div className="item">
+                            <h4>{firstName}</h4>
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
             </article>
         </>
     )
