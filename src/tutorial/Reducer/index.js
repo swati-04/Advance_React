5fbac07cd9b7673f1cf368118500615b6a76cabf
swati-04 <itwa0407@gmail.com>
2021-08-12 12:42:57 +0530

    useState modal

--- src/tutorial/Reducer/index.js
+++ src/tutorial/Reducer/index.js
@@ -1,24 +1,38 @@
-import React ,{useState,useReducer} from 'react'
+import React, { useState, useReducer } from 'react'
 import Modal from './modal';
-import {data} from "../data";
+import { data } from "../data";
 function Index() {
-    const [name,setName]=useState('')
-    const [people,setPeople]=useState(data);
-    const [showModal,setShowModal]=useState(false);
-    const handleSubmit=(e)=>{
+    const [name, setName] = useState('')
+    const [people, setPeople] = useState(data);
+    const [showModal, setShowModal] = useState(false);
+    const handleSubmit = (e) => {
         e.preventDefault();
+        if (name) {
+            setShowModal(true);
+            setPeople([...people, { id: new Date().getTime().toString(), name }])
+            setName('')
+        }
+        else {
+            setShowModal(true)
+        }
+
     }
     return (
-        
         <>
-        {showModal && <Modal/>}
-        <form onSubmit={handleSubmit} className='form'>
-            <div>
-                <input type="text" value={name} onChange={(e)=> setName(e.target.value)}></input>
-            </div>
-            <button type="submit">add user</button>
-        </form>
-            
+            {showModal && <Modal />}
+            <form onSubmit={handleSubmit} className='form'>
+                <div>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                </div>
+                <button type="submit">add user</button>
+            </form>
+            {people.map((person) => {
+                return <>
+                    <div key={person.id}>
+                        <h4>{person.name}</h4>
+                    </div>
+                </>
+            })}
         </>
     )
 }
